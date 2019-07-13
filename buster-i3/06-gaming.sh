87571c15f8f85dea046e0e0161d029f96a8f899c
debxp <debxp.linux@gmail.com>
2019-07-13 17:16:41 -0300

    Add gaming scripts

---
+++ buster-i3/06-gaming.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Steam
+
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install steam
+
+# Lutris
+
+
+# The Battle For Wesnoth
+
+sudo apt install wesnoth
+
+
+echo -e "\nPronto!\n"
