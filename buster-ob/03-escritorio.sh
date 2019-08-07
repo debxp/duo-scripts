ab29886fe0c1e98ec298bbe735f4bf0314e42591
debxp <contato@debxp.org>
2019-08-07 16:47:13 -0300

    Update 03-escritorio.sh

--- buster-ob/03-escritorio.sh
+++ buster-ob/03-escritorio.sh
@@ -2,6 +2,8 @@
 
 sudo apt install libreoffice \
                  libreoffice-l10n-pt-br \
+                 libreoffice-gnome \
                  libreoffice-style-breeze \
                  atril \
-                 goldendict
+                 goldendict \
+                 galculator
