2edc63d4547eb66227169a07b64ac00c5e94e613
debxp <debxp.linux@gmail.com>
2019-08-24 02:03:50 -0300

    Update 99-settings.sh

--- buster-xfce/99-settings.sh
+++ buster-xfce/99-settings.sh
@@ -2,45 +2,46 @@
 
 clear
 
+SCRIPT_PATH=$(dirname "$0")
 
 # Criando pastas
 
-mkdir -p ~/.local/
-mkdir -p ~/.local/opt
+mkdir -p $HOME/.local/
+mkdir -p $HOME/.local/opt
 
 # Artwork
 
 echo -e "\nCopiando fontes e papéis de parede..."
-sudo cp -r artwork/wallpapers /usr/share/backgrounds/
-sudo cp -r artwork/fonts/* /usr/share/fonts/truetype/
+sudo cp -r $SCRIPT_PATH/artwork/wallpapers /usr/share/backgrounds/
+sudo cp -r $SCRIPT_PATH/artwork/fonts/* /usr/share/fonts/truetype/
 sudo fc-cache -f
 
 echo -e "Criando imagem de usuário..."
-cp artwork/icons/tux.png ~/.face
+cp $SCRIPT_PATH/artwork/icons/tux.png $HOME/.face
 
 # Temas de ícones
 
 echo -e "Aplicando temas de ícones..."
-sudo cp -r artwork/icons/debxp-paper-folders /usr/share/icons/
-sudo cp -r artwork/icons/Numix-Circle /usr/share/icons/
-sudo cp -r artwork/icons/Numix-Circle-Light /usr/share/icons/
-sudo cp -r artwork/icons/debxp-diversos /usr/share/icons/
+sudo cp -r $SCRIPT_PATH/artwork/icons/debxp-paper-folders /usr/share/icons/
+sudo cp -r $SCRIPT_PATH/artwork/icons/Numix-Circle /usr/share/icons/
+sudo cp -r $SCRIPT_PATH/artwork/icons/Numix-Circle-Light /usr/share/icons/
+sudo cp -r $SCRIPT_PATH/artwork/icons/debxp-diversos /usr/share/icons/
 
 echo -e "Atualizando cachê de ícones..."
 sudo update-icon-caches /usr/share/icons/debxp-paper-folders
 
 echo -e "Copiando temas do Rofi..."
-sudo cp config/rofi/themes/* /usr/share/rofi/themes/
+sudo cp $SCRIPT_PATH/config/rofi/themes/* /usr/share/rofi/themes/
 
 # Scripts diversos
 
 echo -e "Instalando scripts globais..."
-sudo cp bin/desktop-indicator /usr/bin/debxp-desktop-indicator
-sudo cp bin/rofi-launcher /usr/bin/debxp-rofi-launcher
+sudo cp $SCRIPT_PATH/bin/desktop-indicator /usr/bin/debxp-desktop-indicator
+sudo cp $SCRIPT_PATH/bin/rofi-launcher /usr/bin/debxp-rofi-launcher
 
 # Área de trabalho
 
 echo -e "Configurando a área de trabalho...\n"
-cp -r config ~/.config
+cp -r $SCRIPT_PATH/config $HOME/.config
 
 echo -e "Pronto!\n"
